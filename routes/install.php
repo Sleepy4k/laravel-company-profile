5ad636a2420a4853d9fe7cbd572412a1e6ad2400
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2024-08-28 16:51:41 +0700

    feat(route): add install route and clean up route

---
+++ routes/install.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Install\UserController;
+use App\Http\Controllers\Install\SetupController;
+use App\Http\Controllers\Install\DatabaseController;
+use App\Http\Controllers\Install\FinishedController;
+use App\Http\Controllers\Install\FinalizeController;
+use App\Http\Controllers\Install\PermissionController;
+use App\Http\Controllers\Install\RequirementController;
+
+Route::prefix('install')->group(function () {
+    Route::get('', RequirementController::class)->name('install.requirements');
+    Route::get('permissions', PermissionController::class)->name('install.permissions');
+
+    Route::resource('setup', SetupController::class)->only(['index', 'store'])->names([
+        'index' => 'install.setup',
+        'store' => 'install.setup.store',
+    ]);
+
+    Route::get('database', DatabaseController::class)->name('install.database');
+
+    Route::resource('user', UserController::class)->only(['index', 'store'])->names([
+        'index' => 'install.user',
+        'store' => 'install.user.store',
+    ]);
+
+    Route::get('finalize', FinalizeController::class)->name('install.finalize');
+    Route::get('finish', FinishedController::class)->name('install.finished');
+});
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
-use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Application;
+use App\Http\Controllers\User\ProfileController;
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
@@ -18,9 +18,8 @@ Route::middleware('auth')->group(function () {
         return Inertia::render('Dashboard');
     })->middleware('verified')->name('dashboard');
 
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('profile', ProfileController::class)->only(['edit', 'update', 'destroy']);
 });
 
 require __DIR__.'/auth.php';
+require __DIR__.'/install.php';
