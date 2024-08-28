6865652381ed9547028be729739d1ce4ba2c67b1
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2024-08-28 16:52:44 +0700

    feat(public): add pre setup page and validation for setup

--- public/index.php
+++ public/index.php
@@ -12,6 +12,20 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 // Register the Composer autoloader...
 require __DIR__.'/../vendor/autoload.php';
 
+// Check if environment is missing
+if (!file_exists(__DIR__.'/../.env')) {
+    header('Location: /pre-setup.html?auth=system');
+    exit;
+}
+
+// Check if the application is requiring installation
+if (!file_exists(__DIR__.'/../storage/.installed')) {
+    if (!strpos($_SERVER['REQUEST_URI'], 'install') !== false) {
+        header('Location: /install');
+        exit;
+    }
+}
+
 // Bootstrap Laravel and handle the request...
 (require_once __DIR__.'/../bootstrap/app.php')
     ->handleRequest(Request::capture());
