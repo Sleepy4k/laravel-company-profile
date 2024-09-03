c232e2ba3d777b2b1cef8c35489b5b1a1a2279b8
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2024-09-03 16:03:03 +0700

    chore(route): add application route and remove schedule on default console command

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,10 @@
 use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Application;
+use App\Http\Controllers\DashboardController;
+use App\Policies\Application\SettingTypePolicy;
 use App\Http\Controllers\User\ProfileController;
+use App\Http\Controllers\Application\SettingController;
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
@@ -13,10 +16,13 @@ Route::get('/', function () {
     ]);
 });
 
-Route::middleware('auth')->group(function () {
-    Route::get('/dashboard', function () {
-        return Inertia::render('Dashboard');
-    })->middleware('verified')->name('dashboard');
+Route::prefix('dashboard')->middleware('auth')->group(function () {
+    Route::get('/', DashboardController::class)->name('dashboard.index');
+
+    Route::prefix('application')->group(function () {
+        Route::resource('/', SettingController::class)->names('application')->parameter('', 'application');
+        Route::resource('/type', SettingTypePolicy::class)->names('application.type');
+    });
 
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
