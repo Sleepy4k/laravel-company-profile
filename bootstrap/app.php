316536c6b62f32edc1e4754a33d6f04dabc4bd4f
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2024-09-14 03:48:58 +0700

    chore(bootstrap): register install route

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        then: fn() => require __DIR__.'/../routes/install.php'
     )
     ->withEvents(discover: [
         __DIR__.'/../app/Listeners',
