ef727a0d641032a27740bd4f47937363d276b655
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2024-09-16 23:58:58 +0700

    chore(public): change index.php for better deployment system

--- public/index.php
+++ public/index.php
@@ -4,13 +4,16 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+// Define the base path for the application, so that we can use it in the future (e.g. for deployment)
+$BASE_PATH = __DIR__.'/..';
+
 // Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+if (file_exists($maintenance = $BASE_PATH.'/storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
 // Check if environment is missing
-if (!file_exists(__DIR__.'/../.env')) {
+if (!file_exists($BASE_PATH.'/.env')) {
     if (!strpos($_SERVER['REQUEST_URI'], 'pre-setup') !== false) {
         header('Location: /pre-setup.html');
         exit;
@@ -18,10 +21,10 @@ if (!file_exists(__DIR__.'/../.env')) {
 }
 
 // Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
+require $BASE_PATH.'/vendor/autoload.php';
 
 // Check if the application is requiring installation
-if (!file_exists(__DIR__.'/../storage/.installed')) {
+if (!file_exists($BASE_PATH.'/storage/.installed')) {
     if (!strpos($_SERVER['REQUEST_URI'], 'install') !== false) {
         header('Location: /install');
         exit;
@@ -29,5 +32,5 @@ if (!file_exists(__DIR__.'/../storage/.installed')) {
 }
 
 // Bootstrap Laravel and handle the request...
-(require_once __DIR__.'/../bootstrap/app.php')
+(require_once $BASE_PATH.'/bootstrap/app.php')
     ->handleRequest(Request::capture());
