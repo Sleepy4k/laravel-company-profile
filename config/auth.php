9101f29569354135970e28919881e11ea3590d05
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2024-09-21 17:19:59 +0700

    chore(config): update auth config

--- config/auth.php
+++ config/auth.php
@@ -16,6 +16,7 @@ return [
     'defaults' => [
         'guard' => env('AUTH_GUARD', 'web'),
         'passwords' => env('AUTH_PASSWORD_BROKER', 'users'),
+        'max_attempts' => env('AUTH_MAX_ATTEMPTS', 5),
     ],
 
     /*
