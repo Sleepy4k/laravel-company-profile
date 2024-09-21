28e738da1e8134dad8e05a4ff1d8eecb655a4643
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2024-09-21 11:56:38 +0700

    chore(resource): change edit profile page

--- resources/js/Pages/Profile/Edit.tsx
+++ resources/js/Pages/Profile/Edit.tsx
@@ -7,7 +7,7 @@ import UpdateProfileInformationForm from './Partials/UpdateProfileInformationFor
 export default function Edit({ status }: PageProps<{ status?: string }>) {
     return (
         <AuthenticatedLayout title="Profile">
-            <div className="py-12">
+            <div className="py-6">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
                     <div className="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
                         <UpdateProfileInformationForm className="max-w-xl" />
