027824f425ba2fe2cd133e40bb857ddceab9dcda
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2024-09-21 17:19:41 +0700

    chore(resource): update login page to implement rate limit

--- resources/js/Pages/Auth/Login.tsx
+++ resources/js/Pages/Auth/Login.tsx
@@ -1,15 +1,26 @@
 import { PageProps } from '@/types';
 import trans from '@/utils/translate';
 import alert from '@/utils/sweet.alert';
-import { FormEventHandler } from 'react';
-import { useForm } from '@inertiajs/react';
+import { FormEventHandler, useEffect, useState } from 'react';
+import { Link, useForm } from '@inertiajs/react';
 import TextInput from '@/Components/TextInput';
 import GuestLayout from '@/Layouts/GuestLayout';
 import InputError from '@/Components/InputError';
 import InputLabel from '@/Components/InputLabel';
 import PrimaryButton from '@/Components/PrimaryButton';
 
-export default function Login({ status }: PageProps<{ status?: string }>) {
+interface CurrentPageProps extends PageProps {
+    status?: string;
+    rateLimiter: {
+        max_attempts: number;
+        attempts: number;
+        remaining: number;
+        reset_at: number;
+    };
+}
+
+export default function Login({ status, rateLimiter }: CurrentPageProps) {
+    const [unlockedAt, setUnlockedAt] = useState<number>(rateLimiter.reset_at);
     const langList = {
         'proccessing': trans('page.login.alert.proccessing'),
         'success': trans('page.login.alert.success'),
@@ -20,8 +31,39 @@ export default function Login({ status }: PageProps<{ status?: string }>) {
         password: '',
     });
 
+    useEffect(() => {
+        setUnlockedAt(rateLimiter.reset_at);
+    }, [rateLimiter]);
+
+    useEffect(() => {
+        const timeInterval = setInterval(() => {
+            setUnlockedAt((prev) => {
+                if (prev === 1) {
+                    rateLimiter.remaining = rateLimiter.max_attempts;
+                    rateLimiter.attempts = 0;
+                    clearInterval(timeInterval);
+                    return 0;
+                }
+
+                return prev - 1;
+            });
+        }, 1000);
+
+        return () => clearInterval(timeInterval);
+    }, [rateLimiter]);
+
     const submit: FormEventHandler = (e) => {
         e.preventDefault();
+
+        if (rateLimiter.remaining === 0) {
+            alert.fire({
+                title: 'Whoops!',
+                text: 'You are locked out. Please try again later.',
+                icon: 'error',
+            });
+
+            return;
+        }
 
         post(route('login'), {
             onProgress: () => {
@@ -91,10 +133,34 @@ export default function Login({ status }: PageProps<{ status?: string }>) {
                 </div>
 
                 <div className="flex items-center justify-end mt-4">
-                    <PrimaryButton className="ms-4 mt-2 mb-4" disabled={processing}>
-                        { trans('page.login.button.submit') }
-                    </PrimaryButton>
+                    {rateLimiter.remaining === 0 ? (
+                        <PrimaryButton className="ms-4 mt-2 mb-4" disabled>
+                            Locked out
+                        </PrimaryButton>
+                    ) : (
+                        <PrimaryButton className="ms-4 mt-2 mb-4" disabled={processing}>
+                            { trans('page.login.button.submit') }
+                        </PrimaryButton>
+                    )}
                 </div>
+
+                {/* Add warning for x attempts login */}
+                {rateLimiter.remaining > 0 && rateLimiter.remaining < 4 && (
+                    <div className="mt-4">
+                        <p className="text-sm text-red-600">
+                            {`Warning: You have ${rateLimiter.remaining} login attempts remaining before you are locked out.`}
+                        </p>
+                    </div>
+                )}
+
+                {/* Inform about reset lockdown at */}
+                {rateLimiter.remaining === 0 && (
+                    <div className="mt-4">
+                        <p className="text-sm text-red-600">
+                            {`You are locked out. Please try again in ${unlockedAt} ${unlockedAt > 1 ? 'seconds' : 'second'}.`}
+                        </p>
+                    </div>
+                )}
             </form>
         </GuestLayout>
     );
