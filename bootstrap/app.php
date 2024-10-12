6684deb8292aed6486f0be1078cae669fb0413f2
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2024-10-12 12:48:50 +0700

    chore(bootstrap): register prevent installation middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -33,6 +33,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
             'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
             'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
+            'prevent_installation' => \App\Http\Middleware\PreventInstallationWhenInstalled::class,
         ]);
 
         $middleware->redirectGuestsTo('/');
