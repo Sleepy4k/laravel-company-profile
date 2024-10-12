c275439800d373467bb86db28ee15376516ac015
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2024-10-12 12:50:19 +0700

    chore(config): update project development config

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,14 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: 'resources/js/app.tsx',
-            refresh: true,
-        }),
-        react(),
-    ],
+  plugins: [
+    laravel({
+      input: [
+        'resources/js/app.tsx',
+        'resources/css/app.css'
+      ],
+      refresh: true,
+    }),
+    react(),
+  ],
 });
